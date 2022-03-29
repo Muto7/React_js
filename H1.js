b9acca795f1c016b7a72f33730370db94811a3f7
Muto7 <67232987+Muto7@users.noreply.github.com>
2022-03-29 12:58:12 +0700

    Add files via upload

---
+++ H1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Paragraph = () => {
+  return (
+    <div>
+      <h3>ini dengan nilai variablle</h3>
+    </div>
+  );
+};
+
+export default Paragraph;
---
+++ Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1> ini membuat header dengan class</h1>
+      </div>
+    );
+  }
+}
+
+// export default function Navbar() {
+//   return (
+//     <div>
+//       <h2>ini adalah navbar</h2>
+//     </div>
+//   );
+// }
